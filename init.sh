9fb497958fcd9b1e1e3a6e338c0298fe5abb4152
rgrebenkin <rgrebenkin@mail.ru>
2016-11-15 19:02:01 +0300

    commit

--- init.sh
+++ init.sh
@@ -7,8 +7,8 @@ cp gunicorn.conf ~/web/etc/gunicorn.conf
 sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 cp hello.py ~/web/hello.py
 #sudo ln -sf hello.conf /etc/gunicorn.d/hello.conf
-cd ~/web
+#cd ~/web
 #gunicorn -b 127.0.0.1:8080 hello:application &
 #sudo gunicorn -c ~/web/etc/gunicorn.conf hello:app &
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app &
-sudo /etc/init.d/nginx start
+#sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app &
+#sudo /etc/init.d/nginx start
